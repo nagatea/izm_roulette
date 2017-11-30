17c2506c54e0d20543fca5421d4efbbf29a09e11
nagatech <cheesecakebot@yahoo.co.jp>
2017-12-01 02:14:56 +0900

    タイトルシーン的なもの

--- src/main/TitleScene.java
+++ src/main/TitleScene.java
@@ -7,8 +7,10 @@ package main;
 import core.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 public class TitleScene extends Scene {
+    private BufferedImage titleImage = Main.resource.image.load("test/image/title.png");
     @Override
     public void initialize() {
         super.initialize();
@@ -25,11 +27,12 @@ public class TitleScene extends Scene {
     @Override
     public void draw(Graphics2D g) {
         super.draw(g);
+        g.drawImage(titleImage, 50, 350, 1000, 400, null);
         //g.setColor(Color.white);
         //g.fillRect(0, 0, 600, 800);
         g.setColor(Color.black);
         g.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 100));
-        g.drawString("たいとる", 500, 300);
+        g.drawString("たいとる", 400, 300);
     }
 
     @Override
