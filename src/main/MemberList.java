634f6f59cbc0666cd15fa9083f631f119b06ccab
nagatech <cheesecakebot@yahoo.co.jp>
2017-12-01 22:10:03 +0900

    抽選する内容を管理するMemberListクラスを作成

---
+++ src/main/MemberList.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by nagatea on 2017/12/01.
+ */
+public class MemberList {
+    public ArrayList<String[]> data;
+    public MemberList(){
+        try{
+            File f = new File("test/sample.csv");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+
+            ArrayList<String[]> data = new ArrayList<String[]>();
+            String line = br.readLine();
+            for (int row = 0; line != null ; row++) {
+                data.add(line.split(",", 0));
+                line = br.readLine();
+            }
+            br.close();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
+    public ArrayList<String[]> getMemberList(){
+        return data;
+    }
+}
