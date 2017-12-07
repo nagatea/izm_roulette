24c7e8c7bb0b2f4e205b5f5754dc4562e7ee7ec3
nagatech <cheesecakebot@yahoo.co.jp>
2017-12-07 18:16:07 +0900

    いろいろ追加

--- src/main/MemberList.java
+++ src/main/MemberList.java
@@ -1,25 +1,24 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by nagatea on 2017/12/01.
  */
 public class MemberList {
-    public ArrayList<String[]> data;
+    public ArrayList<String[]> memberList;
     public MemberList(){
         try{
             File f = new File("test/sample.csv");
-            BufferedReader br = new BufferedReader(new FileReader(f));
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f),"Shift-JIS"));
 
-            data = new ArrayList<String[]>();
+            memberList = new ArrayList<String[]>();
             String line = br.readLine();
+            line = br.readLine();
             for (int row = 0; line != null ; row++) {
-                data.add(line.split(",", 0));
+                memberList.add(line.split(",", 0));
                 line = br.readLine();
             }
             br.close();
@@ -28,6 +27,33 @@ public class MemberList {
         }
     }
     public ArrayList<String[]> getMemberList(){
-        return data;
+        return memberList;
+    }
+
+    public void shuffleList(){
+        Collections.shuffle(memberList);
+    }
+
+    public int getSize(){
+        int res = memberList.size();
+        return res;
+    }
+
+    public String[] popList(){
+        String[] res = memberList.remove(0);
+        return res;
+    }
+
+    public String[] getList(){
+        String[] res = memberList.get(0);
+        return res;
+    }
+
+    public String[] getNumber(){
+        String[] res = new String[getSize()];
+        for (int i = 0; i < getSize(); i++){
+            res[i] = memberList.get(i)[0];
+        }
+        return res;
     }
 }
