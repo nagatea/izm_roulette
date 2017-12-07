4c0c28db5b1f044427a0c3e7d32971f0f7092303
nagatech <cheesecakebot@yahoo.co.jp>
2017-12-07 18:16:49 +0900

    ルーレットできるようになった

--- src/main/Roulette.java
+++ src/main/Roulette.java
@@ -5,22 +5,46 @@ import core.SceneManager;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by nagatea on 2017/12/01.
  */
 public class Roulette {
+    public MemberList memberList;
+    public ArrayList<String[]> list;
+    public static String[] data;
+    public String[] number;
+    public String nowNumber = "0";
+    public int remain;
+    public int count;
     public enum Chapter{
-        READY, RUN, STOP, CHANGE,
+        PREPARE, READY, RUN, STOP, CHANGE,
     }
     public static Chapter chapter;
 
     public Roulette(){
-        chapter = Chapter.READY;
+        memberList = new MemberList();
+        list = memberList.getMemberList();
+        remain = memberList.getSize();
+        memberList.shuffleList();
+        chapter = Chapter.PREPARE;
     }
 
     public void step(){
         switch (chapter){
+            case PREPARE:
+                number = memberList.getNumber();
+                if (remain > 1){
+                    data = memberList.popList();
+                    remain--;
+                }else{
+                    data = memberList.getList();
+                }
+                System.out.println(data[0] + data[1] + data[2]);
+                chapter = Chapter.READY;
+                break;
             case READY:
                 if (Main.input.key.isJustPressed(KeyEvent.VK_ENTER)){
                     chapter = Chapter.RUN;
@@ -38,13 +62,18 @@ public class Roulette {
                 break;
             case CHANGE:
                 if (Main.input.key.isJustPressed(KeyEvent.VK_ENTER)){
-                    chapter = Chapter.READY;
+                    chapter = Chapter.PREPARE;
                 }
                 break;
         }
     }
 
     public void draw(Graphics2D g){
+        if (count > 60){
+            count = 0;
+        }else {
+            count++;
+        }
         switch (chapter){
             case READY:
                 g.setColor(Color.BLACK);
@@ -55,11 +84,17 @@ public class Roulette {
                 g.setColor(Color.BLACK);
                 g.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
                 g.drawString("RUN", (int)((float)0.5* Main.FRAMESIZE_W - 155), (int)((float) 0.5*Main.FRAMESIZE_H));
+                if(count%20 == 0){
+                    int i = (int)(Math.random()*(memberList.getSize()));
+                    nowNumber = number[i];
+                }
+                g.drawString(nowNumber, (int)((float)0.5* Main.FRAMESIZE_W - 155), (int)((float) 0.7*Main.FRAMESIZE_H));
                 break;
             case STOP:
                 g.setColor(Color.BLACK);
                 g.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
                 g.drawString("STOP", (int)((float)0.5* Main.FRAMESIZE_W - 155), (int)((float) 0.5*Main.FRAMESIZE_H));
+                g.drawString(data[0], (int)((float)0.5* Main.FRAMESIZE_W - 155), (int)((float) 0.7*Main.FRAMESIZE_H));
                 break;
             case CHANGE:
                 g.setColor(Color.BLACK);
